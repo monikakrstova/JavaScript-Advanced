0ab944b7559fc274ca5f839122843f9486ff42c0
monikakrstova <119454573+monikakrstova@users.noreply.github.com>
2023-01-29 19:33:50 +0100

    Update exercise2.js

--- Homework3/exercise2.js
+++ Homework3/exercise2.js
@@ -49,49 +49,7 @@ $(document).ready(function() {
                     row.appendChild(hairColor);
                     
                     table.appendChild(row);
-                });
-                // headers.forEach(headerContent => {
-                //     let header = document.createElement('th');
-                //     let textNode = document.createTextNode(headerContent);
-                //     header.appendChild(textNode);
-                //     headerRow.appendChild(header);
-                // });
-                // table.appendChild(headerRow);
-
-                // let height = result.getElementsByTagName("th")[0];
-                // let weight = result.getElementsByTagName("th")[1];
-                // let eyeColor = result.getElementsByTagName("th")[2];
-                // let hairColor = result.getElementsByTagName("th")[3];
-                // resultTitle.innerText = `${response.name}`;
-                // height.innerText = `Height: ${response.height}`;
-                // weight.innerText = `Weight: ${response.mass}`;
-                // eyeColor.innerText = `Eye Color: ${response.eye_color}`;
-                // hairColor.innerText = `Hair Color: ${response.hair_color}`;
-                // resultTable.appendChild(document.createElement ('th'));
-
-                // function createColumn (name) {
-                //     let column = document.createElement ('th');
-                //     column.textContent = name;
-                //     return column;
-                // }
-                // function createRow (name) {
-                //     let row = document.createElement ('td');
-                //     row.textContent = name;
-                //     return row;
-                // }
-
-                //  resultTable.appendChild(createColumn('Height'));
-                //  resultTable.appendChild(createColumn('Weight'));
-                //  resultTable.appendChild(createColumn('Eye Color'));
-                //  resultTable.appendChild(createColumn('Hair color'));
-                //  resultTable.appendChild(createRow(`${response.height}`));
-                //  resultTable.appendChild(createRow(`${response.mass}`));
-                //  resultTable.appendChild(createRow(`${response.eye_color}`));
-                //  resultTable.appendChild(createRow(`${response.hair_color}`));
-                
-                
-                
-                
+                });    
             },
             error: function(error){
                 console.log(error);
